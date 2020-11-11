0a55c5acfe9f3d65412e3fbcabb8bdff21b25ab1
doug.w <doug.wright@controlgap.com>
2020-11-11 14:45:18 -0500

    use community react-17 enzyme adapter eslint ignore .test.js files

--- config/setupTests.js
+++ config/setupTests.js
@@ -3,7 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+// import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import '@testing-library/jest-dom/extend-expect'
 
 Enzyme.configure({ adapter: new Adapter() })
